8b875887b42765ec1b3b128cd1868d941db39d2c
yi.gong@hp.com <yi.gong@hp.com>
2013-03-05 10:21:08 +0800

    init template directive copy from ng-include

---
+++ app/js/angularx.js
@@ -0,0 +1,100 @@
+angular.module('app',['time','x']);
+angular.module('time', [])
+  // Register the 'myCurrentTime' directive factory method.
+  // We inject $timeout and dateFilter service since the factory method is DI.
+  .directive('myCurrentTime', function($timeout, dateFilter) {
+    // return the directive link function. (compile function not needed)
+    return function(scope, element, attrs) {
+      var format,  // date format
+          timeoutId; // timeoutId, so that we can cancel the time updates
+ 
+      // used to update the UI
+      function updateTime() {
+        element.text(dateFilter(new Date(), format));
+      }
+ 
+      // watch the expression, and update the UI on change.
+      scope.$watch(attrs.myCurrentTime, function(value) {
+        format = value;
+        updateTime();
+      });
+ 
+      // schedule update in one second
+      function updateLater() {
+        // save the timeoutId for canceling
+        timeoutId = $timeout(function() {
+          updateTime(); // update DOM
+          updateLater(); // schedule another update
+        }, 1000);
+      }
+ 
+      // listen on DOM destroy (removal) event, and cancel the next UI update
+      // to prevent updating time ofter the DOM element was removed.
+      element.bind('$destroy', function() {
+        $timeout.cancel(timeoutId);
+      });
+ 
+      updateLater(); // kick off the UI update process.
+    }
+  });
+
+angular.module('x',[])
+.directive('template',['$http', '$templateCache', '$anchorScroll', '$compile', 
+                 function($http,   $templateCache,   $anchorScroll,   $compile) {
+  return {
+    restrict: 'ECA',
+    terminal: true,
+    compile: function(element, attr) {
+      var srcExp = attr.template || attr.src,
+          onloadExp = attr.onload || '',
+          autoScrollExp = attr.autoscroll;
+
+      return function(scope, element) {
+        var changeCounter = 0,
+            childScope;
+
+        var clearContent = function() {
+          if (childScope) {
+            childScope.$destroy();
+            childScope = null;
+          }
+
+          element.html('');
+        };
+
+        scope.$watch(srcExp, function ngIncludeWatchAction(src) {
+          var thisChangeId = ++changeCounter;
+
+          if (src) {
+            $http.get(src, {cache: $templateCache}).success(function(response) {
+              if (thisChangeId !== changeCounter) return;
+
+              if (childScope) childScope.$destroy();
+              childScope = scope.$new();
+
+              element.html(response);
+              $compile(element.contents())(childScope);
+
+              if (angular.isDefined(autoScrollExp) && (!autoScrollExp || scope.$eval(autoScrollExp))) {
+                $anchorScroll();
+              }
+
+              childScope.$emit('$includeContentLoaded');
+              scope.$eval(onloadExp);
+            }).error(function() {
+              if (thisChangeId === changeCounter) clearContent();
+            });
+          } else clearContent();
+        });
+      };
+    }
+  };
+}]
+
+
+
+
+);
+
+
+
